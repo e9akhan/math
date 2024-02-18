b2a11ec2a12a6b7f438a7f5d9b6309cc7657374d
e9akhan <akbar.khan@enine.school>
2024-02-18 19:06:00 +0530

    Math

---
+++ math074/solution.py
@@ -0,0 +1,57 @@
+"""
+    Math 74
+"""
+
+
+def factorial(n):
+    """
+        Factorial.
+    """
+    fact = 1
+
+    for i in range(2, n+1):
+        fact *= i
+    return fact
+
+
+def solver(n, key):
+    """
+        solver(n, key).
+    """
+    factorials = {
+        str(i): factorial(i)
+        for i in range(10)
+    }
+
+    fact_chain = {}
+
+    for i in range(1, n):
+        num = i
+        fact_list = [i]
+        val = 0
+
+        while True:
+            val = 0
+            for digit in str(num):
+                val += factorials[digit]
+
+            if val in fact_list:
+                break
+
+            fact_list.append(val)
+            num = val
+        
+        length = len(fact_list)
+        fact_chain[length] = fact_chain.get(length, []) + [i]
+
+    return len(fact_chain[key])
+
+def answer():
+    """
+        answer().
+    """
+    return solver(1000000, 60)
+
+
+if __name__ == '__main__':
+    print(f'answer() = {answer()}')
---
+++ math075/solution.py
@@ -0,0 +1,38 @@
+"""
+    Math 75
+"""
+
+def solver(n):
+    """
+        solver().
+    """
+    start = 12
+    int_right_triangle_dict = {}
+
+    for p in range(start, n+1):
+        for a in range(1, p//3):
+            b = (p*p - 2*a*p) // (2*(p-a))
+            c = p - a - b
+
+            if a**2 + b**2 == c**2:
+                int_right_triangle_dict[p] = int_right_triangle_dict.get(p, []) + [(a, b, c)]
+
+                if len(int_right_triangle_dict[p]) > 1:
+                    break
+
+    return len([
+        p
+        for p, lst in int_right_triangle_dict.items()
+        if len(lst) == 1
+    ])
+
+
+def answer():
+    """
+        answer().
+    """
+    return solver(1500000)
+
+
+if __name__ == '__main__':
+    print(f'answer() = {answer()}')
